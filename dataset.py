b735664bb1d345637a3a796869ea267d83be5f21
YuChuXi <cyu245656@gmail.com>
2025-05-02 03:27:36 +0800

    优化缓存

--- dataset.py
+++ dataset.py
@@ -1,11 +1,15 @@
 import os
+import numpy
 import torch
+import logging
 from tqdm import tqdm, trange
 from torch.utils.data import Dataset
 from litemapy import Schematic, Region, BlockState
 
+logger = logging.getLogger(__name__)
+
 LITEMATIC = ".litematic"
-PTH = ".pth"
+SAVED = ".npz" #".pth" # pth保存的非常大
 
 class BlockStateConverter:
     def __init__(self, block_table_path: str, state_table_path: str):
@@ -20,28 +24,39 @@ class BlockStateConverter:
         with open(state_table_path, "r") as f:
             lines = f.readlines()
             self.max_state_num = int(lines.pop(0).split(":")[1])
-            for line in f.readlines():
+            for line in lines:
                 idx, state = line.strip().split("\t")
                 self.state_table[state] = int(idx)
-
-        print(
+                
+        logger.info(
             f"Loaded {len(self.block_table)} blocks and {len(self.state_table)} states"
         )
 
     # 0 is reserved for "none"
     def get_block_idx(self, block: BlockState) -> int:
-        return self.block_table[block.id] # 如果这里报错，说明该更新表了
+        if block.id not in self.block_table:
+            logger.warning(f"Block {block.id} not found in block table")
+            return 0
+        return self.block_table[block.id]
 
     def get_block_state_idx(self, block: BlockState) -> int:
-        return set([self.state_table[s] for s in self.get_block_state(block)]) # 如果这里报错，说明该更新表了
+        if block.id not in self.block_table:
+            logger.warning(f"Block {block.id} not found in block table")
+            return set()
+        states = []
+        for s in self.get_block_state(block):
+            if s not in self.state_table:
+                logger.warning(f"State {s} not found in state table")
+                continue
+            states.append(self.state_table[s])
+        return set(states)
 
     @staticmethod
     def get_block_state(block: BlockState) -> set:
         rawstate = block.to_block_state_identifier()
         if "[" not in rawstate:
-            return set([0])
+            return set()
         return set(rawstate.split("[")[1].split("]")[0].split(","))
-
 
 class LitematicaDataset(Dataset):
     def __init__(self, cache_pth: bool = True, all_in_mem: bool = True):
@@ -57,7 +72,7 @@ class LitematicaDataset(Dataset):
                     for i in os.listdir("dataraw")
                     if i.endswith(LITEMATIC)
                 ]
-                + [i[: -len(PTH)] for i in os.listdir("dataset") if i.endswith(PTH)]
+                + [i[: -len(SAVED)] for i in os.listdir("dataset") if i.endswith(SAVED)]
             )
         )
         self.sample_map_list = {}
@@ -66,19 +81,22 @@ class LitematicaDataset(Dataset):
             return  # early exit!
 
         for s in tqdm(self.sample_name_list, desc="Loading litematics"):
-            if os.path.exists(f"dataset/{s}{PTH}"):
-                try:
-                    if all_in_mem:
-                        self.sample_map_list[s] = torch.load(f"dataset/{s}{PTH}")
-                    continue
-                except Exception as e:
-                    pass
-            map = self.load_litematic(f"dataraw/{s}{LITEMATIC}")
+            try:
+                assert os.path.exists(f"dataset/{s}{SAVED}")
+                if SAVED == ".pth":
+                    map = torch.load(f"dataset/{s}{SAVED}")
+                elif SAVED == ".npz":
+                    map = numpy.load(f"dataset/{s}{SAVED}")["map"]
+            except Exception as e:
+                map = self.load_litematic(f"dataraw/{s}{LITEMATIC}")
 
             if all_in_mem:
                 self.sample_map_list[s] = map
             if cache_pth:
-                torch.save(map.cpu(), f"dataset/{s}{PTH}")
+                if SAVED == ".pth":
+                    torch.save(map, f"dataset/{s}{SAVED}")
+                elif SAVED == ".npz":
+                    numpy.savez_compressed(f"dataset/{s}{SAVED}", map=map)
 
     def load_litematic(self, file):
         schematic = Schematic.load(file)
@@ -97,3 +115,6 @@ class LitematicaDataset(Dataset):
                     assert i < MAX_STATE_NUM, f"Too many states for block {block.id} at {pos}, {state}"
                     map[pos[0], pos[1], pos[2], i+1] = s
         return map
+
+
+
